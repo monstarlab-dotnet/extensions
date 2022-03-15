4a4b00439eb48cb2dce3ed84918e2710a3807b54
Morten Turn Pedersen <morten.turn-pedersen@monstar-lab.com>
2022-03-15 12:47:30 +0100

    Added more unit tests

--- Monstarlab.Extensions/Monstarlab.Extensions.Tests/StringExtensionsTests.cs
+++ Monstarlab.Extensions/Monstarlab.Extensions.Tests/StringExtensionsTests.cs
@@ -51,4 +51,54 @@ public class Tests
 
         Assert.IsNull(parsedValue);
     }
+
+    [Test]
+    public void ToEnumParseSuccess1()
+    {
+        const string valueToParse = "FirstValue";
+
+
+        var parsedValue = valueToParse.ToEnum(TestEnum.RandomValue);
+
+
+        Assert.AreEqual(TestEnum.FirstValue, parsedValue);
+    }
+
+    [Test]
+    public void ToEnumParseSuccess2()
+    {
+        const string valueToParse = "ThirdValue";
+
+
+        var parsedValue = valueToParse.ToEnum(TestEnum.RandomValue);
+
+
+        Assert.AreEqual(TestEnum.ThirdValue, parsedValue);
+    }
+
+    [Test]
+    public void ToEnumParseRandomString()
+    {
+        const string valueToParse = "I'm a random string";
+
+
+        var parsedValue = valueToParse.ToEnum(TestEnum.RandomValue);
+
+
+        Assert.AreEqual(TestEnum.RandomValue, parsedValue);
+    }
+
+    [Test]
+    public void ToEnumParseNullString()
+    {
+        const string? valueToParse = null;
+
+
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+        var parsedValue = valueToParse.ToEnum(TestEnum.RandomValue);
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+
+
+        Assert.AreEqual(TestEnum.RandomValue, parsedValue);
+    }
 }
